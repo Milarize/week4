4343f2587ab2c484962095b40af20e176321cbdc
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-07-28 19:05:28 +0700

    2 Write nest connect Mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
